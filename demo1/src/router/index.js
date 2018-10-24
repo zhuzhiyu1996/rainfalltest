ca9652eb3d1bf12a62b24915675728d45dba3e34
zhuzhiyu1996 <zhiyu.zhu@xbongbong.com>
2018-10-24 20:02:39 +0800

    no message

--- demo1/src/router/index.js
+++ demo1/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index'
+import Login from '@/views/login/login'
 
 Vue.use(Router)
 
@@ -10,6 +11,10 @@ export default new Router({
       path: '/',
       name: 'Index',
       component: Index
+    },{
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
---
+++ demo1/src/views/login/login.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="wrap">
+        <!-- 左半部分图文信息 -->
+        <div class="details"></div>
+
+        <!-- 右边登录注册部分 -->
+        <!-- login -->
+        <div class="login">
+            
+        </div>
+
+        <!-- register -->
+        <div class="register"></div>
+
+    </div>
+</template>
+
+
+<script>
+export default {
+    data () {
+        return{}
+    }
+}
+</script>
+
+
+<style lang="less" scoped>
+
+    .wrap {
+        width: 900px;
+        height: 500px;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        margin-left: -450px;
+        margin-top: -250px;
+        display: flex;
+        border-radius: 8px;
+        box-shadow:
+            0 2px 30px rgba(0, 0, 0, .1), 
+            0 30px 72px 5px rgba(0, 0, 0, .1);
+        overflow: hidden;
+        // 左半部分图文信息
+        .details {
+            width: 550px;
+            height: 500px;
+            background-color: rgba(63, 115, 206, .1);
+        }
+        // 右边登录注册部分
+        .login {
+            width: 350px;
+            height: 500px;
+            background-color: rgba(63, 115, 206, .3);
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+    }
+
+</style>
+
+
