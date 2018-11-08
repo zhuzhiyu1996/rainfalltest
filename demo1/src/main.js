bbc0a858c4aa91bfc175d2244546ab54ab00b452
zhuzhiyu1996 <zhiyu.zhu@xbongbong.com>
2018-11-08 17:04:34 +0800

    element-ui变成按需引入

--- demo1/src/main.js
+++ demo1/src/main.js
@@ -5,13 +5,13 @@ import App from './App'
 import router from './router'
 import iView from 'iview'
 import 'iview/dist/styles/iview.css'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+import { Carousel, CarouselItem } from 'element-ui'
 
 
 Vue.config.productionTip = false
 Vue.use(iView)
-Vue.use(ElementUI)
+Vue.use(Carousel)
+Vue.use(CarouselItem)
 
 
 /* eslint-disable no-new */
