1c2ba0df946a52d89f3dee2dc70f043ed990fb03
zhuzhiyu1996 <zhiyu.zhu@xbongbong.com>
2018-11-13 19:42:27 +0800

    no message

--- demo1/service/index.js
+++ demo1/service/index.js
@@ -1,5 +1,32 @@
+
+const Router = require('koa-router')
+let user = require('./appApi/User')
+
+let router = new Router();
+router.use('/user',user.routes())
+
 const Koa = require('koa')
 const app = new Koa()
+const mongoose = require('mongoose')
+const {connect , initSchemas} = require('./database/init.js')
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+//立即执行函数
+;(async () =>{
+    await connect()
+    initSchemas()
+    const User = mongoose.model('User')
+    let oneUser = new User({userName:'jspang13',password:'123456'})
+    oneUser.save().then(()=>{
+        console.log('插入成功')
+    })
+let  users = await  User.findOne({}).exec()
+console.log('------------------')
+console.log(users)
+console.log('------------------')  
+})()
 app.use(async(ctx)=>{
     ctx.body = '<h1>hello Koa2</h1>'
 })
